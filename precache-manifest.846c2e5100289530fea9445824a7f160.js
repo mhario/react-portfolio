e3b3b626b000a448619c99d3cd0cd62dd2b0aaa8
Mark Hario <mhario@Marks-MacBook-Pro.local>
2020-05-06 23:11:29 -0600

    Updates

---
+++ precache-manifest.846c2e5100289530fea9445824a7f160.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f9837ae63bb5b48525688a022c8e81c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d376bf33ee73007593d",
+    "url": "/static/css/main.e6b1f80d.chunk.css"
+  },
+  {
+    "revision": "598c85791c2e5b0c650e",
+    "url": "/static/js/2.0651fbdb.chunk.js"
+  },
+  {
+    "revision": "846e3710ca5ee3f911296f05447fe08c",
+    "url": "/static/js/2.0651fbdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d376bf33ee73007593d",
+    "url": "/static/js/main.4abcf0db.chunk.js"
+  },
+  {
+    "revision": "4c15ceb5499f6abee85b",
+    "url": "/static/js/runtime-main.eb07c764.js"
+  },
+  {
+    "revision": "67fa2ce838a2bfcd0d7ec58e0e45cc7d",
+    "url": "/static/media/newSkinny.67fa2ce8.jpeg"
+  },
+  {
+    "revision": "091eccff26464d86999dc4179a6c4816",
+    "url": "/static/media/splash.091eccff.jpg"
+  }
+]);
